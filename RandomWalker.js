925d6aaef16419fa3e5054625f8ed65b2ac20950
jdavis30 <jasperdavis95@gmail.com>
2022-10-16 21:14:04 -0400

    Blah

---
+++ RandomWalker.js
@@ -0,0 +1,16 @@
+var x = 200;
+var y = 200;
+
+function setup() {
+    createCanvas(400,400);
+}
+
+function draw() {
+    background(51);
+    stroke(255);
+    strokeWeight(2);
+    point(x,y);
+
+    var r = random(0,4);
+    
+}
